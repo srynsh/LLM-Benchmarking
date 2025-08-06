575a778a6c2aa3f2525f8dfd15e16b02631d8680
Umair Z. Ahmed <umair.z.ahmed@gmail.com>
2025-08-06 16:09:23 +0800

    plot: generator precision predictions by regression model

--- src/plots/plots.py
+++ src/plots/plots.py
@@ -5,6 +5,7 @@ import src.plots.validator_plots as validator_plots
 import src.plots.validator_failure_plots as validator_failure_plots
 import src.plots.repair_plots as repair_plots
 import src.plots.slide_plots as slide_plots
+import src.plots.regression_plots as regression_plots
 import sys
 
 # =========================
@@ -23,6 +24,8 @@ else:
 #   MAIN
 # =========================
 if __name__ == "__main__":
+    regression_plots.regression_plot()
+
     # Elo vs Precision plot
     elo_plots.elo_vs_precision()
     elo_plots.elo_vs_tnr()
