845eeb0bd81f16f15902e72fa05e0ecb8f93afb0
Umair Z. Ahmed <umair.z.ahmed@gmail.com>
2026-02-10 03:41:17 +0800

    Exclude .DS_Store files from data manifest generation

--- generate_data_manifest.sh
+++ generate_data_manifest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # filepath: /Users/umair/projects/codaveri/llm-benchmarking/generate_data_manifest.sh
 
-find data -type f -exec shasum -a 256 {} \; | sort > data_manifest.sha256
+find data -type f ! -name '.DS_Store' -exec shasum -a 256 {} \; | sort > data_manifest.sha256
